facd04dc7e917b8ba1dabe9cb837b153b43753a8
Dmitry Cherkashin <Necromounger1990@gmail.com>
2025-01-25 17:55:37 +0300

    First commit 25.01.2025

---
+++ Lesson_1/A0/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+
+  int N = 0;
+  scanf("%d", &N);
+
+  int max = 0;
+  int var = 0;
+  int counter_for_max = 0;
+  for (int i = 0; i < N; ++i) {
+
+    scanf("%d", &var);
+
+    if (var > max) {
+      max = var;
+      counter_for_max = 1;
+      continue;
+    }
+
+    if(var == max)
+      counter_for_max++;
+  }
+
+  printf("%d", counter_for_max);
+
+  return EXIT_SUCCESS;
+}
