fd0a4c82db14f6bb573fb382ac5f5c37b74240e4
Dmitry Cherkashin <Necromounger1990@gmail.com>
2025-01-26 14:47:13 +0300

    Added Gitignore

--- Lesson_1/A0/main.c
+++ Lesson_1/A0/main.c
@@ -1,25 +1,38 @@
+/* Сколько раз встречается максимум.
+ * На стандартном потоке ввода задаётся натуральное число N (N > 0),
+ * после которого следует последовательность из N целых чисел.
+ * На стандартный поток вывода напечатайте, сколько раз в этой последовательности встречается максимум.
+ * Указание: использовать массивы запрещается.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void)
 {
-  int N = 0;                ///< Переменная согласно условия
+  int N = 0;                ///< Количество чисел
   scanf("%d", &N);
 
-  int max = 0;
-  int var = 0;
-  int counter_for_max = 0;
+  int counter_for_max = 1;        ///< Одно число но будет максимальным
+  int first_var_for_compare = 0;  ///< Первая переменная для временного хранения
+  int second_var_for_compare = 0; ///< Вторая переменная для временного хранения
   for (int i = 0; i < N; ++i) {
-
-    scanf("%d", &var);
-    if (var > max) {
-      max = var;
-      counter_for_max = 1;
+    if (i == 0) {
+      scanf("%d", &first_var_for_compare); ///< Считали первое число единожды
       continue;
     }
 
-    if(var == max)
-      counter_for_max++;
+    scanf("%d", &second_var_for_compare);  ///< Если дошло, то читаем второе число
+
+    if (second_var_for_compare > first_var_for_compare){ ///< Если нашли число больше
+      counter_for_max = 1;                               ///< Счётчик максимальных в 1
+      first_var_for_compare = second_var_for_compare;    ///< Перезаписали числа
+      continue;
+    }
+
+    if (first_var_for_compare == second_var_for_compare)  ///< Если нашли такое же
+      counter_for_max++;                                  ///< Увеличили счётчик
+
   }
 
   printf("%d", counter_for_max);
